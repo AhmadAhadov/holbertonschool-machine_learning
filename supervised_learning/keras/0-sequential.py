1b833572ab28f7744d41429fc38cf1a0902a1c8f
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-08 20:19:59 +0400

    sequential api

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    # 784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95
+    model = tf.keras.Sequential()
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(tf.keras.layers.Dense(
+                input_shape = (nx,),
+                activation=activations[i],
+                kernel_regularizer=tf.keras.regularizers.l2(l2=lambtha)
+            ))
+        else:
+            model.add(tf.keras.layers.Dense(
+                activation=activations[i],
+                kernel_regularizer=tr.keras.regularizers.l2(l2=lambtha)
+            ))
+        if i < len(layers) - 1:
+            model.add(tf.keras.layers.Dropout(1-keep_prob))
+    return model
