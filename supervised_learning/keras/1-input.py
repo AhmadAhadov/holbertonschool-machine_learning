eda48a0ae0571c1ee79df152a211756e69c88640
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-09 14:58:11 +0400

    1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -13,7 +13,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
             layers[i],
             activation=activations[i],
             kernel_regularizer=K.regularizers.l2(l2=lambtha)
-        )
+        )(x)
         if i < len(layers) - 1:
             x = K.layers.Dropout(1-keep_prob)(x)
     model = K.Model(inputs=inputs, outputs=x)
