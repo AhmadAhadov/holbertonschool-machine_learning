404eac196aef35a10dba93990cef543881ba02d7
Ahmad Ahadov <asusahmad26@gmail.com>
2026-08-31 16:07:04 +0400

    1

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -4,13 +4,14 @@ import numpy as np
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     """Implements a full training.
 
     env: initial environment
     nb_episodes: number of episodes used for training
     alpha: the learning rate
     gamma: the discount factor
+    show_result: whether to render the environment every 1000 episodes
     Returns: all values of the score
     """
     weight = np.random.rand(env.observation_space.shape[0], env.action_space.n)
@@ -24,6 +25,8 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         done = False
 
         while not done:
+            if show_result and episode % 1000 == 0:
+                env.render()
             action, grad = policy_gradient(state, weight)
             state, reward, terminated, truncated, _ = env.step(action)
             done = terminated or truncated
