b40be77989c363426d698f8987cf614149afce00
Ahmad Ahadov <asusahmad26@gmail.com>
2026-08-31 16:04:51 +0400

    1

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -8,3 +8,23 @@ def policy(matrix, weight):
     z = matrix.dot(weight)
     exp = np.exp(z - np.max(z, axis=1, keepdims=True))
     return exp / np.sum(exp, axis=1, keepdims=True)
+
+
+def policy_gradient(state, weight):
+    """Computes the Monte-Carlo policy gradient.
+
+    state: matrix representing the current observation of the environment
+    weight: matrix of random weight
+    Returns: the action and the gradient
+    """
+    state = state.reshape(1, -1)
+    probs = policy(state, weight)[0]
+    action = np.random.choice(len(probs), p=probs)
+
+    s = probs.reshape(-1, 1)
+    jacobian = np.diagflat(s) - s.dot(s.T)
+    dsoftmax = jacobian[action]
+    dlog = dsoftmax / probs[action]
+    gradient = state.T.dot(dlog.reshape(1, -1))
+
+    return action, gradient
